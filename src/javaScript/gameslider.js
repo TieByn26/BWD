94c5393de0f6b841550cef2887a666d4e7089a83
TieByn26 <dxuantienn@gmail.com>
2024-05-19 22:11:06 +0700

    update main section game

---
+++ src/javaScript/gameslider.js
@@ -0,0 +1,25 @@
+var TrandingSlider = new Swiper('.tranding-slider', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  autoplay: {
+    delay: 2000, 
+    disableOnInteraction: false, 
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
