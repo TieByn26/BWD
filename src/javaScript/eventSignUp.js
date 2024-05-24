067a30d1e40249d7e0282bf5b826abf79418d1bd
Vuong Ngoc Thuan <vanthuan678310@gmail.com>
2024-05-24 22:11:32 +0700

    fix #4

---
+++ src/javaScript/eventSignUp.js
@@ -0,0 +1,9 @@
+var user_signUp = document.querySelector('.user_signUp');
+var assword_signUp = document.querySelector('.assword_signUp');
+var re_type_password_signUp = document.querySelector('.re_type_password_signUp');
+var email_signUp = document.querySelector('.email_signUp');
+var btn_signUp = document.querySelector('.btn-signUp');
+
+btn_signUp.addEventListener('click', (e)=> {
+ 
+});
