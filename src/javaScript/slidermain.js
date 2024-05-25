724b43e5dd530e84d7fd05ba90189d3ee50916b5
TieByn26 <dxuantienn@gmail.com>
2024-05-25 23:45:31 +0700

    res 1/5

--- src/javaScript/slidermain.js
+++ src/javaScript/slidermain.js
@@ -5,13 +5,25 @@ var swiper = new Swiper(".mySwiper", {
   loop: true,
   slidesPerView: 1,
   effect: 'slide', // Chỉ hiển thị 1 slide tại một thời điểm  
-  autoplay: {
-      delay: 4000, 
-      disableOnInteraction: false, 
-  },
+  // autoplay: {
+  //     delay: 4000, 
+  //     disableOnInteraction: false, 
+  // },
   pagination: {
       el: '.swiper-pagination',
       clickable: true,
   },
-  speed: 800 // Tốc độ chuyển đổi giữa các slide (800ms)
+  speed: 800,
+  mousewheel: {
+    invert: false, // Nếu bạn muốn đảo ngược hướng cuộn, đặt thành true
+  }, 
+   // Tốc độ chuyển đổi giữa các slide (800ms)
+});
+
+document.getElementById('figure-icon--up').addEventListener('click', function() {
+  swiper.slidePrev();
+});
+
+document.getElementById('figure-icon--down').addEventListener('click', function() {
+  swiper.slideNext();
 });
